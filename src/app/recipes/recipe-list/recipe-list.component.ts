8db8a4a5160bfaec5a04aff7e0a458a9aefdd397
Pete <peter.delafosse@gmail.com>
2017-07-25 16:42:55 +1000

    Setting up basic recipe section layouts and model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+	recipes: Recipe[] = [
+		new Recipe('Test Recipe', 'First test recipe', 'https://cdn.pixabay.com/photo/2016/08/25/19/23/jalapeno-1620473_960_720.jpg'),
+		new Recipe('Second Recipe', 'Test recipe #2', 'https://cdn.pixabay.com/photo/2016/08/25/19/23/jalapeno-1620473_960_720.jpg')
+	];
 
-  ngOnInit() {
-  }
+	constructor() {
+
+	}
+
+	ngOnInit() {
+
+	}
 
 }
